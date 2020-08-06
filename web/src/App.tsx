e49fd4970d5978e513530e3fea254d2e9876b4a4
Lara Beatriz <laraamori.machado@gmail.com>
2020-08-05 21:07:53 -0300

    :book: Landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
+
+import Landing from './pages/Landing';
+
+import './assets/styles/global.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello lara</h1>
-    </div>
-  );
+  return <Landing />;
 }
 
 export default App;
